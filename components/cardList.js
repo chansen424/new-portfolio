dd2cea4ff134b647c9d1d7026c69f224a9f22c43
chansen424 <chansen424@yahoo.com>
2020-06-01 21:02:02 -0400

    Control card list wrapping on different screen sizes

--- components/cardList.js
+++ components/cardList.js
@@ -3,12 +3,40 @@ import Card from './card'
 export default function CardList({ cards }) {
   return (
     <>
-      {
-        cards.map((card) => <Card image={card.image}
-          employer={card.employer}
-          role={card.role}
-          description={card.description} />)
-      }
+      <div className="card-list">
+        {
+          cards.map((card) => <Card image={card.image}
+            employer={card.employer}
+            role={card.role}
+            description={card.description} />)
+        }
+      </div>
+
+      <style jsx>
+        {`
+
+        .card-list {
+          display: flex;
+          flex-direction: column;
+        }
+
+        @media only screen and (min-width: 900px) {
+          .card-list {
+            flex-flow: row wrap;
+            justify-content: center;
+            padding: 0rem 4rem;
+          }
+        }
+
+        @media only screen and (min-width: 1000px) {
+          .card-list {
+            flex-flow: row wrap;
+            justify-content: center;
+            padding: 0rem 8rem;
+          }
+        }
+      `}
+      </style>
     </>
   )
 }
