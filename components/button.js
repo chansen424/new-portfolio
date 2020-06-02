4d3feabce91abd240d421e2da34fe325544c65f1
chansen424 <chansen424@yahoo.com>
2020-06-01 21:39:39 -0400

    Allow buttons to be on same line

--- components/button.js
+++ components/button.js
@@ -1,16 +1,12 @@
 export default function Button({ children, href }) {
   return (
     <>
-      <div className="contact-container">
-        <a className="contact" href={href}>{children}</a>
-      </div>
+      <a className="btn" href={href}>{children}</a>
 
       <style jsx>{
-        `.contact-container {
-          margin: 1rem 0rem;
-        }
-
-        .contact {
+        `
+        .btn {
+          margin: 1rem 1rem;
           padding: 0.85em 1.15em;
           border-radius: 3px;
           background: #662E9B;
