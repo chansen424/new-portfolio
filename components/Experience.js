0c8ec811f41c29ac63932124265f41ba37492b41
Christopher Hansen <chansen424@yahoo.com>
2021-07-22 23:05:31 -0400

    Add Experience component

---
+++ components/Experience.js
@@ -0,0 +1,11 @@
+import styles from "./Experience.module.css";
+
+export default function Experience({ employer, role, description }) {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.role}>{role}</h2>
+      <h3 className={styles.employer}>{employer}</h3>
+      <p className={styles.description}>{description}</p>
+    </div>
+  )
+}
